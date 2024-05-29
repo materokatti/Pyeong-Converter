e55a02352c1d1a5c64350770dc953f9bf7ee0b39
Doyun <materokatti@gmail.com>
2024-05-29 10:15:46 +0900

    feature : main converter fuctions in index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export function pyeongToSquareMeters(pyeong: number): number {
+  return pyeong * 3.3058;
+}
+
+export function squareMetersToPyeong(squareMeters: number): number {
+  return squareMeters / 3.3058;
+}
